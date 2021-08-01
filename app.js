febff397f1048ccf1da822300dcd2f19e0b47aea
Nina Prokopova <nina30600@gmail.com>
2021-08-01 18:03:29 +0500

    me: second commit

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ upBtn.addEventListener('click', () => {
 
 downBtn.addEventListener('click', () => {
     changeSlide('down')   
-})  
+}) 
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp'){
+        changeSlide('up')
+    } else if (event.key === 'ArrowDown'){
+        changeSlide('down')
+    }
+})
 
 function changeSlide(direction) {
     if (direction === 'up') {
